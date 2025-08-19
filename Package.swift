c8d0078ee62d127059ae478c4cb95b82a1ca5b01
Tony Smith (smittytone) <tony@smittytone.net>
2025-08-19 20:35:22 +0100

    Switch to SPM verson of clicore

--- Package.swift
+++ Package.swift
@@ -1,17 +1,24 @@
-// swift-tools-version: 5.4
+// swift-tools-version: 5.10
 
 import PackageDescription
 
 let package = Package(
     name: "pdfmaker",
+    dependencies: [
+        .package(url: "https://github.com/smittytone/clicore", branch: "main"),
+    ],
     targets: [
         .executableTarget(
             name: "pdfmaker",
+            dependencies: [
+                .product(name: "Clicore", package: "clicore"),
+            ],
             path: "pdfmaker",
             exclude: [
                 // File not needed for Linux build (so far...)
                 "Info.plist"
-            ]
+            ],
         )
-    ]
+    ],
+
 )
--- pdfmaker/pdfmaker.swift
+++ pdfmaker/pdfmaker.swift
@@ -26,6 +26,7 @@
 
 import Foundation
 import Quartz
+import Clicore
 
 
 struct Pdf {
